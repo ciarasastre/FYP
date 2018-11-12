49616958edef3b78eece22408835f6197955e239
ciarasastre <sastreciara@hotmail.com>
2018-11-12 22:18:00 +0000

    proper variable passing and text area save button

--- static/styles/bookEdit.js
+++ static/styles/bookEdit.js
@@ -1,3 +1,9 @@
+//Declare variables
+var title = "";
+var author = "";
+var chapter = 0;
+var words = "";
+
 function apply()
 {
     var requestURL = 'http://127.0.0.1:5000/test';
@@ -15,14 +21,32 @@ function apply()
 	    //console.log("Books Chapter number is : " + books.chapter_number);
 	    //console.log("Books Text is : " + books.text);
 	    
+	    //Get all required data
+	    title = books.title_name;
+	    author = books.author_name;
+	    chapter = books.chapter_number;
+	    words = books.text;
 
-	    document.getElementById("info_title").innerHTML =  "Book Title : " + books.title_name; 
-	    document.getElementById("info_author").innerHTML =  "Book Author : " + books.author_name;
-	    document.getElementById("info_chpnum").innerHTML =  "Book Chapter number : " + books.chapter_name;
-	    document.getElementById("info_text").innerHTML =  books.text;
+	    //Call function to fill data
+	    fillData(title, author, chapter, words)
     }
 
+}
 
-	console.log("Button works");
+function fillData(title, author, chapter, words)
+{
+	document.getElementById("info_title").innerHTML =  "Book Title : " + title; 
+	document.getElementById("info_author").innerHTML =  "Book Author : " + author;
+	document.getElementById("info_chpnum").innerHTML =  "Book Chapter number : " + chapter
+	document.getElementById("info_text").innerHTML =  words;
+}
 
+function saveBook()
+{
+	console.log("Saved!");
+
+	//Save updated works that were written
+	var textArea = document.getElementById('info_text');
+	var newWords = textArea.value;
+	console.log("New Words are " + newWords);
 }

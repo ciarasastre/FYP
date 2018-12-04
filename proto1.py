ae55c5777008c33863d975064d34a2ed0d00bb90
ciarasastre <sastreciara@hotmail.com>
2018-12-04 15:10:57 +0000

    Added image implementation

--- proto1.py
+++ proto1.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request,jsonify
-from pylatex import Document, Section, Subsection, Command
+from pylatex import Document, Section, Subsection, Command, Figure
 from pylatex.utils import italic, NoEscape
+import os
 import json
 import pprint
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 @app.route("/test")
 def test():
 	# Opening JSON file FOR HTML EDITING
-	return render_template("test.json")
+	return render_template("book.json")
 	#return render_template("test.html");
 
 #Connect to the Site Here:
@@ -31,13 +32,13 @@ def done():
 	#return(newText)
 
 	#Open json file & edit text
-	with open('./templates/test.json', 'r') as editBook:
+	with open('./templates/book.json', 'r') as editBook:
 		data = json.load(editBook)
 
 	temp = data["text"]
 	data["text"] = newText
 
-	with open('./templates/test.json', 'w') as editBook:
+	with open('./templates/book.json', 'w') as editBook:
 		json.dump(data, editBook)
 	return render_template("userPage.html")
 
@@ -58,19 +59,15 @@ def book():
 
 	#Creates .json file in json folder
 	#Uses titlename as JSON File name
-	with open('test.json', 'w') as Book: #open('./json/'+ title_name + '.json', 'w')
+	with open('./templates/book.json', 'w') as Book: #open('./json/'+ title_name + '.json', 'w')
 		json.dump(request.form, Book)
 	return render_template('bookEdit.html')
 
 @app.route("/latex", methods=["GET","POST"])
 def latex():
 	
-	#Actual LaTex Generated Related Code Here
-	from pylatex import Document, Section, Subsection, Command
-	from pylatex.utils import italic, NoEscape
-
 	#Open json file & edit text
-	with open('./templates/test.json', 'r') as PDFBook:
+	with open('./templates/book.json', 'r') as PDFBook:
 		data = json.load(PDFBook)
 
 		#Start pulling data from JSON file
@@ -79,23 +76,34 @@ def latex():
 		chapter_num = data["chapter_number"]
 		book_text = data["text"]
 
-	#return "Book details %s" %(title_name)
-		#if __name__ == '__main__':
-	    # Basic Book Creation with title, author, text
 	book = Document()
 
+	#Images and relative paths
+	scriptDir = os.path.dirname(__file__)
+	imageCoverPath = "harryPotter.jpg"
+	imageBabyPath = "baby.jpg"
+	image_covername = os.path.join(scriptDir, imageCoverPath)
+	image_baby = os.path.join(scriptDir, imageBabyPath)
+
+	#Cover Image
+	with book.create(Figure(position='h!')) as coverImage:
+		coverImage.add_image(image_covername, width='300px') 
 	book.preamble.append(Command('title', title_name)) # Would need users input here
 	book.preamble.append(Command('author', author_name)) # Would need users name
 	book.preamble.append(Command('date', NoEscape(r'\today'))) 
 	book.append(NoEscape(r'\maketitle')) #maketitle is an actual command
 
 	# fill_document(book)
-	book.append(chapter_num + '\n')
+	#book.append(chapter_num + '\n')
+
+	# Add image of baby
+	with book.create(Figure(position='h!')) as image:
+		image.add_image(image_baby, width='200px') 
 	book.append(book_text)
 
-	book.generate_pdf(title_name + 'LaTeXTest', clean_tex=False)
+	book.generate_pdf('./LaTeXFiles/' +title_name+ 'LaTeX', clean_tex=False)
 	tex=book.dumps() # The document as a string in LaTeX syntax
-	book.generate_tex() 
+	#book.generate_tex() 
 	return "Your Book is called %s" %(title_name)
 
 	#LaTex Time
@@ -149,3 +157,9 @@ def latex():
     #book.generate_pdf('LaTeXProto', clean_tex=False)
     #tex=book.dumps() # The document as a string in LaTeX syntax
     #book.generate_tex() 
+
+
+	#image_image1 = os.path.join(os.path.dirname(__file__), 'baby.jpg')
+
+	#with book.create(Figure(position='h!')) as image:
+	#	image.add_image(image_image1, width='100px') #width='400px'

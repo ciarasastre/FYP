1d2dd5fcf8ab559efe0bc3ac318df4fa054bc8a7
ciarasastre <sastreciara@hotmail.com>
2019-02-26 13:18:13 +0000

    Updated

--- proto1.py
+++ proto1.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request,jsonify
-from pylatex import Document, Section, Subsection, Command, Figure
-from pylatex.utils import italic, NoEscape
+from flask import Flask, render_template, request,jsonify, Response
+from pylatex import Document, Section, Subsection, Command, Figure, Package
+from pylatex.utils import bold, italic, NoEscape
 import os
 import json
 import pprint
+import re
 
 app = Flask(__name__)
 
@@ -16,11 +17,30 @@ def test():
 #Connect to the Site Here:
 @app.route("/")
 def hello():
-	return render_template("userPage.html")
+	return render_template("KWHome.html")
 
+#Have all links here:
 @app.route("/home")
 def home():
-	return render_template("userPage.html")
+	return render_template("KWHome.html")
+
+@app.route("/profile")
+def profile():
+	return render_template("KWProfile.html")
+
+@app.route("/library")
+def library():
+	return render_template("KWLibrary.html")
+
+@app.route("/soon")
+def soon():
+	return render_template("KWSoon.html")
+
+@app.route("/editor")
+def editor():
+	return render_template("KWEditor.html")
+
+#End of new links
 
 @app.route("/edit")
 def edit():
@@ -28,19 +48,31 @@ def edit():
 
 @app.route("/done", methods=["GET","POST"])
 def done():
+
+	#PageNav is a string at this point
 	newText = request.form["new_text"]
-	#return(newText)
+	pageNav = request.form["pageNav"]
 
 	#Open json file & edit text
-	with open('./templates/book.json', 'r') as editBook:
+	with open('./templates/json/book.json', 'r') as editBook:
 		data = json.load(editBook)
+	#temp = data["chapters"]["ch1"]["text"]
+	#data["chapters"]["ch1"]["text"] = newText
 
-	temp = data["text"]
-	data["text"] = newText
+	#attempt to create a page 3 blank here
+	#data["name"] = "Lion" 
+	data["chapters"]["ch1"]["p" + pageNav] = newText #str(pageNum) + str(pageNav)
 
-	with open('./templates/book.json', 'w') as editBook:
+
+	newPageNav = (int(pageNav) + 1) # converts to int to make it one value higher
+
+	# Store page nav as a higher (next page instance)
+	temp2 = data["pageNav"]
+	data["pageNav"] = newPageNav
+
+	with open('./templates/json/book.json', 'w') as editBook:
 		json.dump(data, editBook)
-	return render_template("userPage.html")
+	return render_template("KWProfile.html")
 
 #Getting title name back from HTML
 @app.route("/book", methods=["GET","POST"])
@@ -49,34 +81,52 @@ def book():
 	# Build JSON Object from form in HTML
 	title_name = request.form["title_name"] #Get HTML put into new var title_name
 	author_name = request.form["author_name"]
-	chapter_number = request.form["chapter_number"]
-	text = request.form["text"]
+	#chapter_number = request.form["chapter_number"]
 
-	Book = jsonify({"bookTitle" : title_name,
-	"bookAuthor" : author_name,
-	"bookChapter" : chapter_number,
-	"Text" : text }) 
+	#text = request.form["text"] # All text gets put in through here
+
+	# Create new format for JSON Here
+	newBook = {
+		"title_name" : title_name,
+		"author_name" : author_name,
+		#"chapter_number" : chapter_number,
+		"pageNav" : 1,
+		"chapters": 
+			{
+				"ch1" :{
+						#"text" : "",
+						},
+			},
+		}
 
 	#Creates .json file in json folder
 	#Uses titlename as JSON File name
-	with open('./templates/book.json', 'w') as Book: #open('./json/'+ title_name + '.json', 'w')
-		json.dump(request.form, Book)
-	return render_template('bookEdit.html')
+	with open('./templates/json/book.json', 'w') as Book: #open('./json/'+ title_name + '.json', 'w')
+		json.dump(newBook,Book)
+	return render_template('KWEditor.html')
 
-@app.route("/latex", methods=["GET","POST"])
+@app.route("/latex") #, methods=["GET","POST"]
 def latex():
-	
+
+	# VARIABLES
+	boldswitch = 0
+	italicswitch = 0
+	normalswitch = 1
+
+	#FONT
+	impactFont = '<span style=\"font-family: Impact,Charcoal,sans-serif;\">'
+
 	#Open json file & edit text
-	with open('./templates/book.json', 'r') as PDFBook:
+	with open('./templates/json/book.json', 'r') as PDFBook:
 		data = json.load(PDFBook)
 
 		#Start pulling data from JSON file
 		title_name = data["title_name"]
 		author_name = data["author_name"]
-		chapter_num = data["chapter_number"]
-		book_text = data["text"]
+		#chapter_num = data["chapter_number"]
+		book_text = data["chapters"]["ch1"]["p1"]
 
-	book = Document()
+	book = Document(fontenc = 'T1')
 
 	#Images and relative paths
 	scriptDir = os.path.dirname(__file__)
@@ -86,8 +136,8 @@ def latex():
 	image_baby = os.path.join(scriptDir, imageBabyPath)
 
 	#Cover Image
-	with book.create(Figure(position='h!')) as coverImage:
-		coverImage.add_image(image_covername, width='300px') 
+	#with book.create(Figure(position='h!')) as coverImage:
+	#	coverImage.add_image(image_covername, width='300px') 
 	book.preamble.append(Command('title', title_name)) # Would need users input here
 	book.preamble.append(Command('author', author_name)) # Would need users name
 	book.preamble.append(Command('date', NoEscape(r'\today'))) 
@@ -97,14 +147,165 @@ def latex():
 	#book.append(chapter_num + '\n')
 
 	# Add image of baby
-	with book.create(Figure(position='h!')) as image:
-		image.add_image(image_baby, width='200px') 
-	book.append(book_text)
+	#with book.create(Figure(position='h!')) as image:
+	#	image.add_image(image_baby, width='200px') 
+	
 
-	book.generate_pdf('./LaTeXFiles/' +title_name+ 'LaTeX', clean_tex=False)
+	###############################   RULES START HERE ########################################
+	### FONT ###
+	##### TEST FOR CUSTOM / PRETTY FONTS ############
+	#book.packages.append(Package('ascii')) #asciifamily
+	#book.append(NoEscape(r'\asciifamily')) 
+
+	# IMPACT cyklop
+	#book.packages.append(Package('cyklop')) #asciifamily
+	#book.append(NoEscape(r'\normalfont')) 
+
+	# GEORGIA
+	# VERDANA
+	# TAHOMA
+
+	## REMOVAL RULES BEFORE SPLITTING
+
+	####RULE FOR PARAGRAPH
+	book_text = book_text.replace("<p>", "") # remove <p>
+	book_text = book_text.replace("</p>", "\n") # at the end return
+
+	#Dont need </span> tag
+	book_text = book_text.replace("</span", "")
+
+	###RULE FOR FIXING SPACE BUG THIS GOES AT THE END
+	book_text = book_text.replace("<", " <") # place spaces so tags can be read
+	book_text = book_text.replace(">", "> ") 
+
+	
+	for word in book_text.split(): #Iterate through each word
+
+		### RULE FOR BOLD ###
+		if word == '<strong>':
+			boldswitch = 1  #turn switch on
+			normalswitch = 0
+			boldFound = boldRule(book,word,book_text)
+
+		#Is you reached the end turn the switch off
+		if word == '</strong>':
+			boldswitch = 0
+			normalswitch = 1
+			# skip this word here
+
+		### RULE FOR ITALICS ###
+		if word == '<em>':
+			italicswitch = 1  #turn switch on
+			normalswitch = 0
+			italicFound = italicRule(book,word,book_text)
+
+		#Is you reached the end turn the switch off
+		if word == '</em>':
+			italicswitch = 0
+			normalswitch = 1
+			# skip this word here
+
+		## RULE FOR FONT ##
+		if word == '<span':
+			normalswitch = 0
+
+		#Run font function
+		normalswitch = fontRule(book, word, normalswitch)
+
+		#IF normal switch is on continue printing words
+		if normalswitch == 1:
+			book.append(word + ' ')#print the word and a space word +
+
+	# Iterating words is over <-- Here
+
+	# Generate PDF
+	book.generate_pdf('./LaTeXFiles/' +title_name+'LaTeX', clean_tex=False)
 	tex=book.dumps() # The document as a string in LaTeX syntax
-	#book.generate_tex() 
+	book.generate_tex() 
 	return "Your Book is called %s" %(title_name)
+
+############# RULES #################
+def fontRule(book, word, normalswitch):
+	# IMPACT
+	if word == 'Impact,Charcoal,sans-serif;\">':
+		book.packages.append(Package('cyklop')) #asciifamily
+		book.append(NoEscape(r'\normalfont'))
+		normalswitch = 1
+
+	# ARIAL
+	if word == 'Arial,Helvetica,sans-serif;\">':
+		book.append(NoEscape(r'\sffamily')) #Sansserrif
+		normalswitch = 1
+
+	# TIMES NEW ROMAN
+	if word == 'Roman,Times,serif,-webkit-standard;\">':
+		book.append(NoEscape(r'\rmfamily'))
+		normalswitch = 1
+
+	return(normalswitch)
+
+def boldRule(book,word,book_text):
+	#bold rules:
+	startbold = "<strong>"
+	endbold = "</strong>"
+	boldWord = book_text[book_text.find(startbold) + len(startbold):book_text.find(endbold)] # middle word
+	book.append(bold(boldWord))
+	return()
+
+def italicRule(book,word,book_text):
+	#italic rules:
+	startitalic = "<em>"
+	enditalic = "</em>"
+	italicWord = book_text[book_text.find(startitalic) + len(startitalic):book_text.find(enditalic)] # middle word
+	book.append(italic(italicWord))
+	return()
+
+@app.route("/HPDemo")
+def testing():
+	# Basic Book Creation with title, author
+	book = Document()
+	title_name = "Testing"
+	book.preamble.append(Command('title', "title_name")) # Would need users input here
+	book.preamble.append(Command('author', "author_name")) # Would need users name
+	book.append(NoEscape(r'\maketitle')) #maketitle is an actual command
+
+	#Attempting to apply italics
+	book.append("Applying")
+	italicFound = italics(book)
+	book.append("is really hard...")
+
+	#Attempting to apply Bold
+	book.append("\n" + "Applying")
+	boldFound = bolds(book)
+	book.append("was not workin for me...")
+
+	#Attempting to apply Underline
+	#book.append(LineBreak())
+	book.append("\n" + "Applying")
+	underLinesFound = underlines(book)
+	book.append("was the worst of all")
+
+	book.generate_pdf('./LaTeXFiles/' +title_name+'LaTeX', clean_tex=False)
+	tex=book.dumps() # The document as a string in LaTeX syntax
+	book.generate_tex() 
+	return "Your Book is created hopefully %s" %(title_name)
+
+#YOU CAN PASS BOOK WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+def italics(book):
+	book.append(italic(' italics '))
+	return()
+
+def bolds(book):
+	book.append(bold(" bold "))
+	return()
+
+def underlines(book):
+	#book.append(verbatim("underline")) #\\underline{content_separator}")
+	#book.append("underline{escape} ")
+	#book.append(underline(" underline "))
+	verbatim = ("Setting \\underline{escape} to \\underline{False}")
+	book.append(verbatim)
+	return()
 
 	#LaTex Time
 
--- static/styles/bookEdit.js
+++ static/styles/bookEdit.js
@@ -1,8 +1,9 @@
 //Declare variables
 var title = "";
 var author = "";
-var chapter = 0;
+var chapter = "";
 var words = "";
+
 
 function apply()
 {
@@ -16,39 +17,97 @@ function apply()
     {
 	    var booksText = request.response;
 	    var books = JSON.parse(booksText);
-	    //console.log("Books Title name is : " + books.title_name);
-	    //console.log("Books Author is : " + books.author_name);
-	    //console.log("Books Chapter number is : " + books.chapter_number);
 	    //console.log("Books Text is : " + books.text);
 	    
 	    //Get all required data
 	    title = books.title_name;
 	    author = books.author_name;
 	    chapter = books.chapter_number;
-	    words = books.text;
+	    pageNav = books.pageNav;
 
-	    //Call function to fill data
-	    fillData(title, author, chapter, words)
+	    p0 = books.chapters.ch1.text; //problem here with hardcoding
+
+	    // This will need to store an array of pages to then iterate over when editing
+
+	    fillData(title, author, chapter, p0, pageNav)
     }
 
 }
 
-function fillData(title, author, chapter, words)
+function fillData(title, author, chapter, p1, pageNav)
 {
 	document.getElementById("info_title").innerHTML =  "Book Title : " + title; 
 	document.getElementById("info_author").innerHTML =  "Book Author : " + author;
-	document.getElementById("info_chpnum").innerHTML =  "Book Chapter number : " + chapter
-	document.getElementById("info_text").innerHTML =  words;
+	document.getElementById("info_chpnum").innerHTML =  "Book Chapter number : " + chapter;
+	document.getElementById("info_text").innerHTML =  p0;
+	document.getElementById("info_pageNav").value =  pageNav;
 }
 
-function saveBook()
+function saveBook(pages)
 {
 	console.log("Saved!");
 
-	//Save updated works that were written
-	var textArea = document.getElementById('info_text');
-	var newWords = textArea.value;
-	console.log("New Words are " + newWords);
-	document.getElementById("newWords").innerHTML =  newWords;
+	console.log("Pages contains ")
 
+	var pageContents = ["p0"];
+
+	console.log(pageContents[0]);
+
+	// pages[1] for example would be
+	// pages[1] = books.chapters.ch1.p1;
+
+	//Save updated works that were written
+	//var textArea = document.getElementById('info_text');
+	//var newWords = textArea.value;
+	//console.log("New Words are " + newWords);
+	//document.getElementById("newWords").innerHTML =  newWords;
+}
+
+//Goes to the next page of the book
+function nextPage()
+{
+
+	//You need to save P1 and move onto P2
+	document.getElementById("info_text").innerHTML =  p0;
+	
+}
+
+//Goes to the previous page of the book
+function prevPage()
+{
+	//The text are needs to change from P1(text) to P2
+	document.getElementById("info_text").innerHTML =  p0;
+
+	//You need to save P1 and move onto P2
+}
+
+function bold() 
+{
+	//document.getElementsByTagName("input").style.fontWeight = 'bold';
+	//console.log("Bold Works");
+    //document.getElementById("info_text").style.fontWeight = 'bold'; 
+
+    //If its already bold put it back to normal
+
+	if(document.getElementById("info_text").style.fontWeight == 'bold')
+	{
+		document.getElementById("info_text").style.fontWeight = "normal";
+	}
+	else
+	{
+		document.getElementById("info_text").style.fontWeight = 'bold';
+	}
+}
+
+function italic() 
+{
+	//If its already italics put it back to normal
+	if(document.getElementById("info_text").style.fontStyle == "italic")
+	{
+		document.getElementById("info_text").style.fontStyle = "normal";
+	}
+	else
+	{
+		document.getElementById("info_text").style.fontStyle = "italic";
+	}
 }
